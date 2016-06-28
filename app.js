146fc9a8db6c3f4320b5b9d8d727127facdf5ad8
Jeremy <jkaffrey@gmail.com>
2016-06-28 14:39:45 -0600

    Finished project

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+var app           = require('express')();
+var cookieSession = require('cookie-session');
+                    require('dotenv').load();
+
+app.use(cookieSession({
+  name: 'Cookies2',
+  keys: [
+    process.env.KEY_1,
+    process.env.KEY_2,
+    process.env.KEY_3
+  ]
+}));
+
+app.get('/', function(req, res) {
+
+  if(req.session.views) req.session.views = parseInt(req.session.views, 10) + 1;
+  else req.session.views = 1;
+
+  res.end('<html>You\'ve visited this page ' + (req.session.views) + ' times. <br /><button onclick="location.reload();">Refresh</button>&nbsp||&nbsp<a href="./logout">Clear Views</a></html>');
+});
+
+app.get('/logout', function(req, res) {
+
+  req.session = null;
+  res.end('<a href=\'/\'>Start Again</a>');
+});
+
+app.listen(8080);
